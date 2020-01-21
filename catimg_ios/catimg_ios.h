d7e8ca80beaca5f2ed89139eaefca47a3aa88365
Terry Chou <terrychoucn@gmail.com>
2020-01-21 12:47:21 +0800

    Initial Commit

---
+++ catimg_ios/catimg_ios.h
@@ -0,0 +1,19 @@
+//
+//  catimg_ios.h
+//  catimg_ios
+//
+//  Created by Terry Chou on 1/21/20.
+//  Copyright © 2020 Boogaloo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for catimg_ios.
+FOUNDATION_EXPORT double catimg_iosVersionNumber;
+
+//! Project version string for catimg_ios.
+FOUNDATION_EXPORT const unsigned char catimg_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <catimg_ios/PublicHeader.h>
+
+
